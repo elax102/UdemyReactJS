37bd59dccce602878fb3827f69385270047ee3fb
Alex Davis <davis.alex.jr@gmail.com>
2015-10-22 11:51:03 -0400

    Finished dropdown functions. Dropdown will toggle open/close. Selected Item will become title and have active class.

--- dropdown/src/dropdown.jsx
+++ dropdown/src/dropdown.jsx
@@ -6,18 +6,42 @@ var React = require('react');
 var Button = require('./button');
 var ListItem = require('./list-item');
 
+//toggle state of this.state.open when clicked
+//setState should rerender for us
 module.exports = React.createClass({
     handleClick: function(){
-        alert('dropdown');
+        this.setState({open: !this.state.open})
     },
+    
+    getInitialState: function(){
+    return {open: false}
+    },
+    
+    handleItemClick: function(item){
+        this.setState({
+            open: false,
+            itemTitle: item
+        });
+    },
+    
     render: function(){
     var list = this.props.items.map(function(item){
-        return <ListItem item={item} />
-    });
+        return <ListItem 
+            item={item} 
+            whenItemClicked={this.handleItemClick} 
+            className={this.state.itemTitle === item ? "active" : ""}
+        />
+    }.bind(this));
     
+        //if this.state.open is truthy display show if falsy display empty string
+        //when clicked, toggle this.state.open and force rerender
         return <div className="dropdown">
-        <Button whenClicked={this.handleClick} className="btn-default" title={this.props.title} subTitleClassName="caret" />
-        <ul>{list}</ul>
+        <Button 
+            whenClicked={this.handleClick} 
+            className="btn-default" 
+            title={this.state.itemTitle || this.props.title} subTitleClassName="caret" 
+        />
+        <ul className={"dropdown-menu " + (this.state.open ? "show" : "")}>{list}</ul>
         
         </div>
     }
